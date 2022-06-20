c3be713c7ee3ce2dee2e3ea07a0787bc98f3ed34
Charles Srstka <support@charlessoft.com>
2022-06-19 23:26:46 -0400

    Add performance tests to Swift package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,29 @@
-// swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "CSProgress",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CSProgress",
-            targets: ["CSProgress"]),
+            targets: ["CSProgress"]
+        ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CSProgress",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "CSProgressTests",
-            dependencies: ["CSProgress"]),
+            dependencies: ["CSProgress"]
+        ),
+        .executableTarget(
+            name: "CSProgressPerformanceTests",
+            dependencies: ["CSProgress"],
+            path: "Tests/CSProgressPerformanceTests"
+        )
     ]
 )
